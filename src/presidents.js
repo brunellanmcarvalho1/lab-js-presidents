de1bb1381a469aa399f016b13b9f2182ebdd17bb
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2024-10-28 23:15:11 +0100

    Solved lab

--- src/presidents.js
+++ src/presidents.js
@@ -412,54 +412,47 @@ const presidents = [
     tookOffice: 2021,
     leftOffice: null,
     party: "Democratic",
-  }
+  },
 ];
 
-
-
-
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map((president) => president.name);
+}
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
 
-
-
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter(
+    (presidentsArr) => presidentsArr.party === "Democratic"
+  );
+}
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
 
-
-
+function countYearsInOffice(presidentsArr) {
+  const totalYears = presidents.leftOffice - presidents.tookOffice;
+  return presidentsArr.reduce((totalYears, presidents) => {
+    if (presidents.leftOffice !== null) {
+      return totalYears + (presidents.leftOffice - presidents.tookOffice);
+    }
+    return totalYears;
+  }, 0);
+}
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
-
-
-
+function sortPresidentsByBirthYear(presidentsArr) {
+  return presidentsArr.sort((a, b) => a.birthYear - b.birthYear);
+}
 
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
 function getAgeAtInauguration(presidentsArr) {}
 
-
-
-
 // Bonus: Iteration 6 | Presidents Born After - `filter()`
 function getPresidentsBornAfter(presidentsArr, year) {}
-
-
-
 
 // Bonus: Iteration 7 | Count Republican Presidents
 function countRepublicanPresidents(presidentsArr) {}
 
-
-
-
 // Bonus: Iteration 8 | Sort Presidents by Name - `sort()`
 function sortPresidentsByName(presidentsArr) {}
-
